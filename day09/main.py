edc1a8abec86a67a60fd640783666cc371fdc3ae
António Carneiro <amcc@fe.up.pt>
2024-12-11 22:48:04 +0000

    Updates

--- day09/main.py
+++ day09/main.py
@@ -16,20 +16,44 @@ def defragment_block(id_defrag, pos_block, pos_start, files, empty):
 
     return pos_block, pos_start
 
-def process_empty_space(id_defrag, id_defrag_end, pos_block, pos_start, files, empty):
-    print("emtpy space ", pos_start, "  size : ", empty[pos_start])
+def fill_empty_space(id_defrag, pos_block, pos_start, files, empty):
+    print("fillin position : ", pos_start, " with space : ", empty[pos_start])
+    found = False
     while empty[pos_start] > 0 and pos_block > pos_start:
-        print("  block ", pos_block, "  size : ", files[pos_block])
-        if (files[pos_block] < empty[pos_start]):
+        print("  trying to fill block position : ", pos_block, " with space : ", files[pos_block])
+        if (files[pos_block] > 0 and files[pos_block] <= empty[pos_start]):
+            found = True
+            print("  block position : ", pos_block, " with space : ", files[pos_block], " can be moved")
             id_defrag.extend([pos_block for _ in range(files[pos_block])])
             empty[pos_start] -= files[pos_block]
             files[pos_block] = 0
             pos_block -= 1
         else:
             pos_block -= 1
-            id_defrag_end = [pos_block for _ in range(empty[pos_block])] + id_defrag_end
 
-    return pos_block, pos_start
+    return pos_block, pos_start, found
+
+def move_file(id_defrag, pos_block, files, empty, empty_new_files):
+    pos_start = 0
+    found = False
+    while not found and pos_start < pos_block:
+        if empty[pos_start] > files[pos_block]:
+            found = True
+            empty[pos_start] -= files[pos_block]
+            empty_new_files[pos_start] = (pos_block, files[pos_block])
+        else:
+            pos_start += 1
+
+def build_id_defrag(files, empty, emtpy_new_files):
+    id_defrag = []
+    pos = 0
+    for iblock in range(len(files)):
+        id_defrag.append([iblock for _ in range(files[iblock])])
+        empty_size = empty[iblock]
+        for ifile in emtpy_new_files:
+            id_defrag.append([ifile for _ in range(files[iblock])])
+
+    return id_defrag
 
 def compute_checksum(id_defrag):
     return sum(i * idx for idx, i in enumerate(id_defrag))
@@ -52,14 +76,13 @@ if __name__ == '__main__':
 
     elif part == '2':
         pos_start = 0
-        pos_block = len(files) - 1
-        id_defrag = [pos_start for _ in range(files[pos_start])]
-        id_defrag_end = []
-        while pos_start < pos_block:
-            pos_block, pos_start = process_empty_space(id_defrag, id_defrag_end, pos_block, pos_start, files, empty)
+        id_defrag = []
+        found = True
+        while found:
+            id_defrag = id_defrag + [pos_start for _ in range(files[pos_start])]
+            pos_block = len(files) - 1
+            pos_block, pos_start, found = fill_empty_space(id_defrag, pos_block, pos_start, files, empty)
             pos_start += 1
             print(id_defrag)
-            print(id_defrag_end)
 
-        print(id_defrag_end, id_defrag)
-        print("Result of part 2: ", compute_checksum(id_defrag + id_defrag_end))
+        print("Result of part 2: ", compute_checksum(id_defrag))
--- day11/main.py
+++ day11/main.py
@@ -1,5 +1,7 @@
 import os
 import sys
+from functools import lru_cache
+
 sys.path.append(os.path.join(os.path.dirname(__file__), '../utils'))
 
 from utils import get_input_filename, read_input_file
@@ -18,6 +20,42 @@ def transform_stone(x):
     else:
         return [str(int(x) * 2024)]
 
+def search(x, depth, stones_cache):
+    if (x, depth) in stones_cache:
+        return stones_cache[(x, depth)]
+
+    else:
+        if depth == 0:
+            return 1
+        else:
+            if x == '0':
+                res = search('1', depth - 1, stones_cache)
+            elif len(x) % 2 == 0:
+                a = str(int(x[0:int(len(x)/2)]))
+                b = str(int(x[int(len(x)/2):]))
+                res = search(a, depth - 1, stones_cache) + search(b, depth - 1, stones_cache)
+            else:
+                res = search(str(int(x) * 2024), depth - 1, stones_cache)
+
+            stones_cache[(x, depth)] = res
+            return res
+
+@lru_cache(maxsize=32)
+def search_with_cache(x, depth):
+    if depth == 0:
+        return 1
+    else:
+        if x == '0':
+            res = search('1', depth - 1, stones_cache)
+        elif len(x) % 2 == 0:
+            a = str(int(x[0:int(len(x)/2)]))
+            b = str(int(x[int(len(x)/2):]))
+            res = search(a, depth - 1, stones_cache) + search(b, depth - 1, stones_cache)
+        else:
+            res = search(str(int(x) * 2024), depth - 1, stones_cache)
+
+        return res
+
 def blink(stones):
     return [y for x in [transform_stone(stone) for stone in stones] for y in x]
 
@@ -34,7 +72,8 @@ if __name__ == '__main__':
 
     elif part == '2':
         stones = data
-        for i in range(75):
-            stones = blink(stones)
+        stones_cache = {}
+        n_blinks = 75
 
-        print("Result of part 2: ", len(stones))
+        print("Result of part 2        : ", sum(search(stone, n_blinks, stones_cache) for stone in stones))
+        print("Result of part 2 (cache): ", sum(search_with_cache(stone, n_blinks) for stone in stones))
