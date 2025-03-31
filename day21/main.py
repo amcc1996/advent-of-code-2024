c6c8636c813736b1a9193e1f8769c4782f88cf62
António Carneiro <amcc@fe.up.pt>
2025-03-31 14:42:00 +0100

    Working implementation, missing result

--- day21/main.py
+++ day21/main.py
@@ -1,6 +1,13 @@
 import os
 import sys
 
+import re
+import functools
+import itertools
+import copy
+
+from collections import deque
+
 sys.path.append(os.path.join(os.path.dirname(__file__), '../utils'))
 
 from utils import get_input_filename, read_input_file
@@ -147,12 +154,12 @@ def compute_sequence_buttons(code, map_number_and_position_to_directions, map_di
     return min_dir_code_me
 
 def compute_sequence_buttons_part2(code, map_number_and_position_to_directions, map_direction_and_position_to_directions):
+    """Impossible.
+    """
     n_intermediate_robots = 2
 
     num_code = code
     dir_code_robot_1_list = parse_num_code(num_code, map_number_and_position_to_directions)
-    print(1, len(code))
-    print(len(dir_code_robot_1_list), len(dir_code_robot_1_list[0]))
 
     dir_code_robot_i_input_list = [x for x in dir_code_robot_1_list]
     for i in range(n_intermediate_robots):
@@ -177,6 +184,124 @@ def compute_sequence_buttons_part2(code, map_number_and_position_to_directions, 
             min_dir_code_me = dir_code_me
 
     return min_dir_code_me
+
+def convert_dir_code_to_blocks(dir_code):
+    """This function converts a direction code to a list of blocks ending in A.
+    Powered by co-pilot.
+
+    Returns the code in a dicitonary with keys the blocks and values the number of times
+    they appear in the parsed code.
+    """
+    # Step 1: Split after each 'A' using negative lookbehind
+    splits = [x for x in re.split(r'(?<=A)', dir_code) if x]
+
+    # Step 2: Handle case where dir_code doesn't end in 'A'
+    if not dir_code.endswith('A'):
+        splits[-1] = splits[-1] + 'A'
+
+    blocks = {}
+    for split in splits:
+        if split in blocks:
+            blocks[split] += 1
+        else:
+            blocks[split] = 1
+
+    return blocks
+
+@functools.cache
+def parse_dir_code_block_memoized(dir_code):
+    """This code parses the direction code block with memoization. It is similar to before
+    but now returns each parsed code as a list of blocks ending in A and not as a string.
+    In addition, this function is only expeceted to be called with a block that ends in A
+    with maximum length 5.
+
+    It returns a list of dictionaries where each dictionary contains the blocks and the number
+    of times they appear in the parsed code.
+    """
+    parsed_code_list = ['']
+    start_pos = 'A'
+    for char in dir_code:
+        parsed_char_list = map_direction_and_position_to_directions[char][start_pos]
+        aux = []
+        for code in parsed_code_list:
+            for parsed_char in parsed_char_list:
+                aux.append(code + parsed_char)
+        parsed_code_list = [x for x in aux]
+        start_pos = char
+
+    parsed_code_blocks = []
+    for parsed_code in parsed_code_list:
+        parsed_blocks = {}
+        for block, n_repeat in convert_dir_code_to_blocks(parsed_code).items():
+            if block in parsed_blocks:
+                parsed_blocks[block] += n_repeat
+            else:
+                parsed_blocks[block] = n_repeat
+        parsed_code_blocks.append(parsed_blocks)
+
+    return parsed_code_blocks
+
+def parse_dir_code_blocks(blocks):
+    parsed_individual_blocks = []
+    for block in blocks:
+        aux = copy.deepcopy(parse_dir_code_block_memoized(block))
+        for i in range(len(aux)):
+            for key in aux[i]:
+                aux[i][key] = aux[i][key] * blocks[block]
+
+        parsed_individual_blocks.append(aux)
+
+    parsed_blocks_list = []
+    for combination in itertools.product(*parsed_individual_blocks):
+        aux = {}
+        for parsed_block in combination:
+            for block in parsed_block:
+                if block in aux:
+                    aux[block] += parsed_block[block]
+                else:
+                    aux[block] = parsed_block[block]
+
+        parsed_blocks_list.append(aux)
+
+    return parsed_blocks_list
+
+def length_dir_code_block(blocks):
+    total_length = 0
+    for block, count in blocks.items():
+        total_length += len(block) * count
+
+    return total_length
+
+def compute_minimal_complexity_part2_using_blocks_dfs(num_code, n_intermediate_pads=25):
+    """Impossible.
+    """
+    num = int(code.split('A')[0])
+    depth = 0
+
+    dir_code_robot_1_list = parse_num_code(num_code, map_number_and_position_to_directions)
+    dir_code_robot_1_list_blocks = [convert_dir_code_to_blocks(x) for x in dir_code_robot_1_list]
+
+    queue = deque([(x, length_dir_code_block(x), depth) for x in dir_code_robot_1_list_blocks])
+    min_length = float('inf')
+    count = 0
+    while queue:
+        blocks, length, depth = queue.pop()
+        count += 1
+        print(count, depth)
+        if depth == n_intermediate_pads:
+            if length < min_length:
+                min_length = length
+            continue
+
+        if length >= min_length:
+            continue
+
+        parsed_blocks = parse_dir_code_blocks(blocks)
+        for parsed_block in parsed_blocks:
+            queue.append((parsed_block, length_dir_code_block(parsed_block), depth + 1))
+
+    print(num, min_length)
+    return num * min_length
 
 def print_map(map):
     for key in map:
@@ -204,9 +329,9 @@ if __name__ == '__main__':
     ]
 
     map_number_and_position_to_directions = initialise_map_number_and_position_to_directions()
-    print_map(map_number_and_position_to_directions)
+    # print_map(map_number_and_position_to_directions)
     map_direction_and_position_to_directions = initialise_map_direction_and_position_to_directions()
-    print_map(map_direction_and_position_to_directions)
+    # print_map(map_direction_and_position_to_directions)
 
     if part == '1':
         res = 0
@@ -218,10 +343,30 @@ if __name__ == '__main__':
         print("Result of part 1: {0}".format(res))
 
     elif part == '2':
+        # Debug
+        # -----
+        # dir_code = '<vA>^AAv<<A>>^AvA<^A>Av<<A>>^AvA^A' # Easy after two parse
+        dir_code = '>>vA^A' # Easy
+        # dir_code = 'vAA<A>^A<A>A' # Easy after one parse
+        # print("Dir code\n{0}".format(dir_code))
+        # print("Dir code blocks")
+        # print(convert_dir_code_to_blocks(dir_code))
+        # print("Dir code parsed")
+        # print(parse_dir_code(dir_code, map_direction_and_position_to_directions))
+        # print("Dir code parsed blocks [0]")
+        # print(convert_dir_code_to_blocks(parse_dir_code(dir_code, map_direction_and_position_to_directions)[0]))
+        # print("Dir code parsed blocks dictionaries")
+        # print(parse_dir_code_blocks(convert_dir_code_to_blocks(dir_code)))
+        # print("Length of the original dir code blocks")
+        # print(length_dir_code_block(convert_dir_code_to_blocks(dir_code)))
+        # print("Length of the parsed dir code blocks")
+        # print(dir_code)
+        # print(convert_dir_code_to_blocks(dir_code))
+        # print(length_dir_code_block(parse_dir_code_blocks(convert_dir_code_to_blocks(dir_code))[0]))
+        # print(parse_dir_code_blocks(convert_dir_code_to_blocks(dir_code))[0])
+
         res = 0
         for code in data:
-            sequence = compute_sequence_buttons_part2(code, map_number_and_position_to_directions, map_direction_and_position_to_directions)
-            res += compute_complexity(code, sequence)
-            print("Code: {0}, Sequence: {1}, Length: {2}, Num: {3}".format(code, sequence, len(sequence), int(code.split('A')[0])))
+            res += compute_minimal_complexity_part2_using_blocks_dfs(code, n_intermediate_pads=6)
 
-        print("Result of part 1: {0}".format(res))
+        print("Result of part 2: {0}".format(res))
