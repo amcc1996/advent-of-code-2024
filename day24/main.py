7b7f6823b8f387c647b906fee6101f8986ef9d86
António Carneiro <amcc@fe.up.pt>
2025-05-23 08:00:42 +0100

    Started remembering day 24

--- day24/main.py
+++ day24/main.py
@@ -61,6 +61,95 @@ def run_circuit(wires, gates):
 
     return compute_number(wires)
 
+def find_all_wires(wires, gates):
+    all_wires = set()
+    for wire in wires:
+        all_wires.add(wire)
+
+    for gate in gates:
+        wire_input1, wire_input2, op, wire_output = gate
+        all_wires.add(wire_input1)
+        all_wires.add(wire_input2)
+        all_wires.add(wire_output)
+
+    return all_wires
+
+def extract_output_wires(all_wires):
+    output_wires = []
+    for wire in all_wires:
+        if wire[0] == 'z':
+            output_wires.append(wire)
+
+    output_wires.sort(key=lambda x: int(x[1:]))
+
+    return output_wires
+
+def find_output_gate_dependency(output_wire, gates):
+    output_gate_dependency_id = set()
+    queue = set()
+    queue.add(output_wire)
+    while queue:
+        wire = queue.pop()
+        for i_gate, (wire_input1, wire_input2, op, wire_output) in enumerate(gates):
+            if wire == wire_output:
+                output_gate_dependency_id.add(i_gate)
+                if wire_input1[0] not in ['x', 'y']:
+                    queue.add(wire_input1)
+                if wire_input2[0] not in ['x', 'y']:
+                    queue.add(wire_input2)
+
+                break
+
+    return output_gate_dependency_id
+
+def check_bit_sum(i_bit, n_bits, gates):
+    initial_wires = {}
+    for i in range(n_bits):
+        initial_wires['x' + str(i)] = 0
+        initial_wires['y' + str(i)] = 0
+
+    output_wire_1 = 'z' + str(i_bit)
+    output_wire_2 = 'z' + str(i_bit + 1)
+
+    # x = 0, y = 0
+    wires = initial_wires.copy()
+    run_circuit(initial_wires, gates)
+    if not (wires[output_wire_1] == 0 and wires[output_wire_2] == 0):
+        return False
+
+    # x = 1, y = 0
+    wires = initial_wires.copy()
+    wires['x' + str(i_bit)] = 1
+    run_circuit(initial_wires, gates)
+    if not (wires[output_wire_1] == 1 and wires[output_wire_2] == 0):
+        return False
+
+    # x = 0, y = 1
+    wires = initial_wires.copy()
+    wires['y' + str(i_bit)] = 1
+    run_circuit(initial_wires, gates)
+    if not (wires[output_wire_1] == 1 and wires[output_wire_2] == 0):
+        return False
+
+    # x = 1, y = 1
+    wires = initial_wires.copy()
+    wires['x' + str(i_bit)] = 1
+    wires['y' + str(i_bit)] = 1
+    run_circuit(initial_wires, gates)
+    if not (wires[output_wire_1] == 0 and wires[output_wire_2] == 1):
+        return False
+
+    return True
+
+def find_wrong_bits(n_bits, gates):
+    wrong_bits = set()
+    for i_bit in range(n_bits):
+        print("Checking bit {0}".format(i_bit))
+        if not check_bit_sum(i_bit, n_bits, gates):
+            wrong_bits.add(i_bit)
+
+    return wrong_bits
+
 if __name__ == '__main__':
     filename, part = get_input_filename(os.path.dirname(__file__))
     data = read_input_file(filename)
@@ -71,9 +160,19 @@ if __name__ == '__main__':
 
         print("Result of part 1: {0}".format(res))
 
-    # elif part == '2':
-    #     computers = build_computers(data)
-    #     groups = find_computer_groups(computers)
-    #     res = find_largest_group(groups)
+    elif part == '2':
+        n_bits = sum([1 for wire in wires if wire[0] == 'x'])
+        all_wires = find_all_wires(wires, gates)
+        output_wires = extract_output_wires(all_wires)
+        # print("Output wires: {0}".format(output_wires))
+        output_wires_dependency_id = [find_output_gate_dependency(x, gates) for x in output_wires]
+        # for output_wire, output_wire_dependency_id in zip(output_wires, output_wires_dependency_id):
+        #     output_wire_dependency = [gates[x][3] for x in output_wire_dependency_id]
+        #     print("Output wire {0} dependency: {1}".format(output_wire, output_wire_dependency))
 
-    #     print("Result of part 2: {0}".format(res))
+
+        print(find_wrong_bits(n_bits, gates))
+
+        res = 0
+
+        print("Result of part 2: {0}".format(res))
